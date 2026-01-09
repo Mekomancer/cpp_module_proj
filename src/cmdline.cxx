2ece50300217f287ea025278b5ac7b229bc91f63
Mark R <mark.z.r@live.com>
2026-01-08 16:15:12 -0800

    small patches

--- src/cmdline.cxx
+++ src/cmdline.cxx
@@ -7,7 +7,7 @@ void print_version();
 int quick_parse_args(int argc, char *argv[]){
   for(int i = 0; i < argc; ++i){
     string arg = argv[i];
-    if(arg=="--help" || arg == "-h)"){
+    if(arg=="--help" || arg == "-h"){
       print_help();
       return 1;
     } else if(arg=="--version" || arg == "-V"){
@@ -20,6 +20,10 @@ int quick_parse_args(int argc, char *argv[]){
 
 void print_help(){
   print_version();
+  std::print("\nOPTIONS:\n");
+  std::print("\t-h --help       Print this message\n");
+  std::print("\t-V --version    Print version info\n");
+  
 }
 
 void print_version(){
